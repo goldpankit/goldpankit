f823edc123bf6e21b41625569eef4c01c5a7aee1
Caesar Liu <ck9300001@qq.com>
2023-05-19 00:15:16 +0800

    增加服务空间搜索接口

--- node-service/routes/service.space.js
+++ node-service/routes/service.space.js
@@ -1,5 +1,6 @@
 const request = require('../utils/request.define')
 request.post('/service/space/create').proxy()
 request.get('/service/space/:spaceId').proxy()
+request.get('/service/space/search').proxy()
 
 module.exports = request.router
