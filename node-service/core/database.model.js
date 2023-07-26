9ffc942ff16f30efd664f4677d1485d57d2fea89
Caesar Liu <ck9300001@qq.com>
2023-07-26 22:02:24 +0800

    完善模型修改接口

--- node-service/core/database.model.js
+++ node-service/core/database.model.js
@@ -1,17 +1,19 @@
 const cache = require('./utils/cache')
+const utils = require('./utils/index')
 module.exports = {
   // 删除模型
-  delete (databaseId, modelName) {
+  delete (databaseId, modelId) {
     const database = cache.databases.get(databaseId)
     if (database == null) {
       throw new Error(`Can not found database by id ${databaseId}`)
     }
     if (database.models != null) {
-      const index = database.models.findIndex(m => m.name === modelName)
-      if (index !== -1) {
-        database.models.splice(index, 1)
-        cache.databases.save(database)
+      const index = database.models.findIndex(m => m.id === modelId)
+      if (index === -1) {
+        throw new Error(`Can not found model by id ${modelId}`)
       }
+      database.models.splice(index, 1)
+      cache.databases.save(database)
     }
   },
   // 保存模型
@@ -23,12 +25,15 @@ module.exports = {
     if (database.models == null) {
       database.models = []
     }
+    // 验证名称
     const model = database.models.find(m => m.name === newModel.name)
     if (model != null) {
-      throw new Error(`the model named ${newModel.name} already exists.`)
+      throw new Error(`The model named ${newModel.name} already exists.`)
     }
+    newModel.id = utils.generateId()
     database.models.push(newModel)
     cache.databases.save(database)
+    return newModel.id
   },
   // 保存模型
   update (databaseId, newModel) {
@@ -39,10 +44,11 @@ module.exports = {
     if (database.models == null) {
       database.models = []
     }
-    const model = database.models.find(m => m.name === newModel.name)
+    const model = database.models.find(m => m.id === newModel.id)
     if (model == null) {
-      throw new Error(`Can not found model named ${newModel.name}`)
+      throw new Error(`Can not found model by id ${newModel.id}`)
     }
+    Object.assign(model, newModel)
     cache.databases.save(database)
   }
 }
