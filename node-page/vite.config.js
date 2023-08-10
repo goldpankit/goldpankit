f0392e689e67137065bd721158ef40a91488713c
Caesar Liu <ck9300001@qq.com>
2023-08-10 23:50:40 +0800

    恢复配置

--- node-page/vite.config.js
+++ node-page/vite.config.js
@@ -30,8 +30,7 @@ export default ({ mode }) => {
           rewrite: (path) => path.replace(new RegExp(`^${localApiPrefix}`), "")
         },
         [remoteApiPrefix]: {
-          // target: 'http://192.168.124.5:10088',
-          target: 'http://112.74.58.58',
+          target: 'http://192.168.124.5:10088',
           changeOrigin: true,
           rewrite: (path) => path.replace(new RegExp(`^${remoteApiPrefix}`), "")
         },
