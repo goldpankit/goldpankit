f66f06cef1a201463ac796f57e1edf329845f3ce
Caesar Liu <ck9300001@qq.com>
2024-06-23 20:20:08 +0800

    线弧度备份

--- node-page/src/components/database/query-model/DesignerV2.vue
+++ node-page/src/components/database/query-model/DesignerV2.vue
@@ -11,6 +11,15 @@
 
 <script>
 import Konva from 'konva'
+// 变量放在data中会导致卡顿
+const c = {
+  stage: null,
+  elementLayer: null,
+  // 预览stage
+  previewStage: null,
+  // 元素layer克隆
+  cloneElementLayer: null
+}
 export default {
   name: 'DesignerV2',
   props: {
@@ -20,31 +29,95 @@ export default {
     return {
       scale: 1,
       elementLayer: null,
-      elements: []
+      elements: [],
+      lines: []
     }
   },
   methods: {
     // 拖拽表
-    handleDrop () {
+    handleDrop (e) {
+      // 获取放下时的鼠标坐标
+      c.stage.setPointersPositions(e);
+      const mousePos = c.stage.getPointerPosition();
       // 获取到表信息
-      console.log(this.model.dragData)
+      c.createTable(this.model.dragData, mousePos.x - 100, mousePos.y)
+      // 重新绘制预览
+      this.__redrawPreview()
+    },
+    computeLinePositions ({ field1, field2, table1, table2 }) {
+      let leftTable = table1
+      let rightTable = table2
+      if (table1.absolutePosition().x > table2.absolutePosition().x) {
+        leftTable = table2
+        rightTable = table1
+      }
+      let leftField = field1
+      let rightField = field2
+      if (field1.absolutePosition().x > field2.absolutePosition().x) {
+        leftField = field2
+        rightField = field1
+      }
+      // 计算第一个坐标点
+      const firstPoint = [
+        leftField.absolutePosition().x + 200,
+        leftField.absolutePosition().y + 15
+      ]
+      // 计算两个group之间的距离
+      const distance = rightField.absolutePosition().x - leftField.absolutePosition().x - 200
+      // 计算高度差引起的转弯点
+      let turnPoint1 = [...firstPoint]
+      let turnPoint2 = [...firstPoint]
+      const heightDifference = Math.abs(rightField.absolutePosition().y - leftField.absolutePosition().y)
+      if (heightDifference > 0) {
+        // 箭头转弯点1
+        turnPoint1 = [
+          leftField.absolutePosition().x + 200 + distance / 2,
+          leftField.absolutePosition().y + 15
+        ]
+        turnPoint2 = [
+          leftField.absolutePosition().x + 200 + distance / 2,
+          rightField.absolutePosition().y + 15
+        ]
+      }
+      // 计算最后一个坐标点
+      const lastPoint = [
+        firstPoint[0] + distance,
+        rightField.absolutePosition().y + 15
+      ]
+      let data = '';
+      data += 'M' + firstPoint[0] + ',' + firstPoint[1] + ' ';  // 移动到第一个点
+      // data += 'L' + turnPoint1[0] + ',' + turnPoint1[1] + ' ';  // 绘制到第二个点
+      data += 'Q' + (turnPoint1[0]) + ',' + (turnPoint1[1]) + ' ';  // 绘制到第二个点
+      data += 'L' + turnPoint2[0] + ',' + turnPoint2[1] + ' ';  // 绘制到第二个点
+      data += 'L' + lastPoint[0] + ',' + lastPoint[1] + ' ';  // 绘制到第二个点
+      return data
+      // return [...firstPoint, ...turnPoint1, ...turnPoint2, ...lastPoint]
+    },
+    // 重新绘制预览
+    __redrawPreview () {
+      // 移除layer
+      c.cloneElementLayer.destroy()
+      // c.previewStage.remove(c.cloneElementLayer)
+      // 重新克隆layer并添加
+      c.cloneElementLayer = c.elementLayer.clone()
+      c.previewStage.add(c.cloneElementLayer)
     }
   },
   mounted () {
-    const stage = new Konva.Stage({
+    c.stage = new Konva.Stage({
       container: '.stage',
       width: 2000,
       height: 2000,
-      draggable: true
+      draggable: false
     })
     // 添加布局layer
     const backgroundLayer = new Konva.Layer();
-    stage.add(backgroundLayer);
+    c.stage.add(backgroundLayer);
     // - 添加500个点平均分布
     const cycleLength = 80
     const cycleRadius = 1
     // (总宽 - 总直径) / 数量 + 1
-    let distance = (stage.width() - cycleLength * cycleRadius * 2) / (cycleLength + 1);
+    let distance = (c.stage.width() - cycleLength * cycleRadius * 2) / (cycleLength + 1);
     for (let i = 0; i < cycleLength; i++) {
       for (let j = 0; j < cycleLength; j++) {
         const ball = new Konva.Circle({
@@ -61,26 +134,26 @@ export default {
     }
 
     // 添加内容布局
-    window.elementLayer = new Konva.Layer();
-    stage.add(elementLayer);
-
+    c.elementLayer = new Konva.Layer();
+    c.stage.add(c.elementLayer);
     // 当前拖拽的字段
     let currentDragField = null
+    let currentTable = null
     // 所有的表
     const tables = []
     // 创建表
-    function createTable (name, x, y) {
-      const table = new Konva.Group({
-        name: 'table',
+    c.createTable = (table, x, y) => {
+      // 创建表分组
+      const tableGroup = new Konva.Group({
+        name: `table_${table.name}_${Math.round(Math.random() * 10000)}`,
         x,
         y,
         draggable: true
       })
-      tables.push(table)
-      elementLayer.add(table);
+      tables.push(tableGroup)
+      c.elementLayer.add(tableGroup);
       // 创建标题背景
       const titleBackground = new Konva.Rect({
-        name: 'a' + Math.round(Math.random() * 10000),
         x: 0,
         y: 0,
         width: 200,
@@ -91,10 +164,9 @@ export default {
       })
       // 创建标题
       const title = new Konva.Text({
-        name: 'a' + Math.round(Math.random() * 10000),
         x: 10,
         y: 3,
-        text: name,
+        text: table.name,
         fontSize: 20,
         fontFamily: 'Calibri',
         fill: '#fff',
@@ -108,18 +180,16 @@ export default {
         height: 50
       })
       // 创建字段
-      const fields = ['name', 'gender', 'mobile', 'email', 'address']
       const fieldHeight = 30
-      for (let i = 0; i < fields.length; i++) {
-        const field = fields[i]
+      for (let i = 0; i < table.fields.length; i++) {
+        const field = table.fields[i]
         const fieldGroup = new Konva.Group({
-          name: 'a' + Math.round(Math.random() * 10000),
+          name: 'field',
           x: 0,
           y: titleBackground.height() + fieldHeight * i
         })
         // 背景
         const fieldBackground = new Konva.Rect({
-          name: 'a' + Math.round(Math.random() * 10000),
           x: 0,
           y: 0,
           width: 200,
@@ -130,10 +200,9 @@ export default {
         })
         // 文字
         const fieldText = new Konva.Text({
-          name: 'a' + Math.round(Math.random() * 10000),
           x: 25,
           y: 0,
-          text: field,
+          text: field.name,
           fontSize: 14,
           fontFamily: 'Calibri',
           fill: '#fff',
@@ -148,29 +217,86 @@ export default {
           radius: 4,
           fill: '#00D2FF'
         })
-        fieldGroup.add(fieldBackground);
-        fieldGroup.add(fieldText);
-        fieldGroup.add(fieldDragBall);
-        table.add(fieldGroup);
         // 为拖拽小球添加鼠标按下事件，将对象记录至currentDragField
         fieldDragBall.on('mousedown', () => {
-          table.draggable(false)
-          stage.draggable(false)
-          currentDragField = fieldDragBall
+          tableGroup.draggable(false)
+          c.stage.draggable(false)
+          currentDragField = fieldGroup
+          currentTable = tableGroup
         })
         // 悬浮在拖拽小球上时修改鼠标样式
         fieldDragBall.on('mouseover', () => {
-          stage.container().style.cursor = 'pointer'
+          c.stage.container().style.cursor = 'pointer'
         })
+        // 为字段组添加鼠标悬浮事件
+        fieldGroup.on('mouseover', () => {
+          // 当currentDragField不为null时，修改背景色为红色
+          if (currentDragField && currentTable !== tableGroup) {
+            fieldBackground.fill('#fc6a70')
+          }
+        })
+        // 为字段组添加鼠标离开事件
+        fieldGroup.on('mouseout', () => {
+          // 当currentDragField不为null时，恢复背景色
+          if (currentDragField) {
+            fieldBackground.fill('#232325')
+          }
+        })
+        // 为字段组添加鼠标松开事件
+        fieldGroup.on('mouseup', () => {
+          // 当currentDragField不为null时，从currentDragField绘制箭头到当前小球位置
+          if (currentDragField && currentTable !== tableGroup) {
+            // 绘制箭头
+            const points = this.computeLinePositions({
+              field1: currentDragField,
+              field2: fieldGroup,
+              table1: currentTable,
+              table2: tableGroup
+            })
+            currentDragField.__line = fieldGroup.__line = {
+              table1: currentTable,
+              field1: currentDragField,
+              table2: tableGroup,
+              field2: fieldGroup,
+              line: new Konva.Path({
+                name: `line_${Math.round(Math.random() * 10000)}`,
+                data: points,
+                stroke: '#fc6a70',
+                strokeWidth: 1
+              })
+            }
+            // 添加到layer
+            c.elementLayer.add(currentDragField.__line.line);
+            // 恢复背景色
+            fieldBackground.fill('#232325')
+            // 重新绘制预览
+            this.__redrawPreview()
+          }
+        })
+        // 添加到字段分组
+        fieldGroup.add(fieldBackground);
+        fieldGroup.add(fieldText);
+        fieldGroup.add(fieldDragBall);
+        // 添加到表
+        tableGroup.add(fieldGroup);
       }
-      table.add(titleBackground);
-      table.add(title);
+      // 添加到表分组
+      tableGroup.add(titleBackground);
+      tableGroup.add(title);
+      // 为表添加拖拽移动事件
+      tableGroup.on('dragmove', () => {
+        const fields = tableGroup.find('.field')
+        fields.forEach((field) => {
+          // 调整箭头位置
+          if (field.__line) {
+            field.__line.line.data(this.computeLinePositions(field.__line));
+          }
+        })
+      })
     }
-    createTable('user', 100, 100)
-    createTable('role', 400, 100)
     // 创建一条隐藏的虚线
     const line = new Konva.Line({
-      name: 'a' + Math.round(Math.random() * 10000),
+      name: 'dash_line',
       points: [0, 0, 0, 0],
       stroke: '#fc6a70',
       strokeWidth: 1,
@@ -178,62 +304,66 @@ export default {
       lineJoin: 'round',
       dash: [10, 10]
     })
-    elementLayer.add(line);
+    c.elementLayer.add(line);
 
     // 添加预览stage
-    const previewStage = new Konva.Stage({
+    c.previewStage = new Konva.Stage({
       container: '.preview-stage',
       width: 200,
       height: 200,
       scaleX: 1 / 10,
       scaleY: 1 / 10,
     })
-    const cloneElementLayout = elementLayer.clone({ listening: false });
-    previewStage.add(cloneElementLayout)
+    c.cloneElementLayer = c.elementLayer.clone({ listening: false });
+    c.previewStage.add(c.cloneElementLayer)
     // - 更新预览内容
     function updatePreview() {
       // 找到所有节点，更新节点的位置
-      elementLayer.children.forEach((shape) => {
-        const clone = cloneElementLayout.findOne('.' + shape.name());
+      c.elementLayer.children.forEach((shape) => {
+        const clone = c.cloneElementLayer.findOne('.' + shape.name());
         clone.position(shape.position());
+        if (clone instanceof Konva.Path) {
+          clone.data(shape.data());
+          clone.strokeWidth(2)
+        }
+        console.log('clone', clone)
       })
     }
-    stage.on('dragmove', updatePreview);
+    c.stage.on('dragmove', updatePreview);
 
     // 滚动缩放处理
     const scaleBy = 1.05;
-    stage.on('wheel', (e) => {
+    c.stage.on('wheel', (e) => {
       if (!e.evt.ctrlKey) {
         return
       }
       // 阻止默认事件，防止页面滚动
       e.evt.preventDefault();
-      const oldScale = elementLayer.scaleX()
-      const pointer = stage.getPointerPosition();
+      const oldScale = c.elementLayer.scaleX()
+      const pointer = c.stage.getPointerPosition();
       const mousePointTo = {
-        x: (pointer.x - elementLayer.x()) / oldScale,
-        y: (pointer.y - elementLayer.y()) / oldScale,
+        x: (pointer.x - c.elementLayer.x()) / oldScale,
+        y: (pointer.y - c.elementLayer.y()) / oldScale,
       };
       // 计算新的缩放比例
       const newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
       if (newScale > 3 || newScale < 0.2) {
         return
       }
-      elementLayer.scale({ x: newScale, y: newScale });
-      cloneElementLayout.scale({ x: newScale, y: newScale });
+      c.elementLayer.scale({ x: newScale, y: newScale });
+      c.cloneElementLayer.scale({ x: newScale, y: newScale });
       // 缩放时重新计算坐标并赋值
       const newPos = {
         x: pointer.x - mousePointTo.x * newScale,
         y: pointer.y - mousePointTo.y * newScale,
       };
-      elementLayer.position(newPos);
-      cloneElementLayout.position(newPos);
+      c.elementLayer.position(newPos);
+      c.cloneElementLayer.position(newPos);
     })
 
     // 鼠标松开
-    stage.on('mouseup touchend', (e) => {
-      // 设置stage和所有的表均可拖动
-      stage.draggable(true)
+    c.stage.on('mouseup touchend', (e) => {
+      // 设置所有的表均可拖动
       tables.forEach(table => table.draggable(true))
       // 清空拖拽字段
       currentDragField = null
@@ -242,25 +372,25 @@ export default {
     })
 
     // 鼠标移动
-    stage.on('mousemove touchmove', (e) => {
+    c.stage.on('mousemove touchmove', (e) => {
       // 如果存在currentDragField，则将隐藏的虚线line绘制到鼠标移动的当前位置
       if (currentDragField) {
-        // 获取currentDragField的在stage中的x和y
-        const pos = stage.getPointerPosition();
+        // 获取currentDragField的在c.stage中的x和y
+        const pos = c.stage.getPointerPosition();
+        // console.log('mouse', pos)
+        // console.log('currentDragField x', currentDragField.absolutePosition().x)
+        // console.log('scale', c.elementLayer.scale())
+        line.scale({ x: 1 / c.elementLayer.scale().x, y: 1 / c.elementLayer.scale().y })
+        line.zIndex(10000)
         line.points([
           // 第一个点坐标
-          currentDragField.absolutePosition().x - stage.x(),
-          currentDragField.absolutePosition().y - stage.y(),
+          currentDragField.absolutePosition().x,
+          currentDragField.absolutePosition().y,
           // 当前鼠标位置
-          pos.x - stage.x(),
-          pos.y - stage.y()
+          pos.x,
+          pos.y
         ])
       }
-    })
-
-    // 接收拖拽内容
-    stage.on('drop', (e) => {
-      console.log('e', e)
     })
   }
 }
