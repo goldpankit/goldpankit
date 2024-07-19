5e88ed84eeb07932972595038dd90ddbed10d02e
Caesar Liu <ck9300001@qq.com>
2024-07-20 04:10:09 +0800

    更换升级镜像地址

--- node-service/core/client.js
+++ node-service/core/client.js
@@ -16,16 +16,16 @@ module.exports = {
             return
           }
           // 执行升级命令
-          const upgradeCommand = `npm uninstall goldpankit -g && npm install goldpankit@${latestVersion.versionNo} -g --registry https://registry.npmjs.org`
-          const upgradeCommandTip = `npm uninstall goldpankit -g\nnpm install goldpankit@${latestVersion.versionNo} -g --registry https://registry.npmjs.org\nOR\nnpm uninstall goldpankit -g\nsudo npm install goldpankit@${latestVersion.versionNo} -g --registry https://registry.npmjs.org`
+          const upgradeCommand = `npm install goldpankit@${latestVersion.versionNo} -g --registry https://registry.npmmirror.com`
+          const upgradeCommandTip = `npm install goldpankit@${latestVersion.versionNo} -g --registry https://registry.npmmirror.com\nOR\nsudo npm install goldpankit@${latestVersion.versionNo} -g --registry https://registry.npmmirror.com`
           log.tip(`正在自动升级, 请稍等...`)
           node.exec(process.cwd(), upgradeCommand)
             .then(() => {
-              log.success(`已升级至 v${latestVersion.versionNo}。现在你可以重新执行 'kit' 命令来打开KIT。如果反复出现此消息，请尝试手动执行以下命令：\n\`\`\`shell\n${upgradeCommandTip}\n\`\`\``)
+              log.success(`已升级至 v${latestVersion.versionNo}。现在你可以重新执行 'kit' 命令来打开KIT。如果反复出现此消息，请尝试手动执行以下命令：\n\`\`\`\n${upgradeCommandTip}\n\`\`\``)
               resolve(true)
             })
             .catch(e => {
-              log.error(`自动升级失败. 你可以手动执行以下命令来进行升级。\n\`\`\`\n${upgradeCommandTip}\n\`\`\`\n如果总是升级失败，请尝试使用超级管理员权限或更换node版本！`)
+              log.error(`自动升级失败. 你可以手动执行以下命令来进行升级。\n\`\`\`\n${upgradeCommandTip}\n\`\`\`\n如果多次升级失败，请尝试使用超级管理员权限或更换node版本！`)
               reject(e)
             })
         })
