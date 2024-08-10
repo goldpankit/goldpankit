5572d4ec85c5de1a112111277527878248461bad
Caesar Liu <ck9300001@qq.com>
2024-08-10 16:24:24 +0800

    增加差异表达式测试；增加预置插件安装

---
+++ node-service/tests/diff-express/index.js
@@ -0,0 +1,6 @@
+const diffExpress = require('../../core/utils/diff-express')
+const fs = require('../../core/utils/fs')
+const root = process.cwd()
+const sourceFileContent = fs.readFile(`${root}/source.txt`).content
+const expressFileContent = fs.readFile(`${root}/express.txt`).content
+console.log(diffExpress.merge(expressFileContent, sourceFileContent).content)
