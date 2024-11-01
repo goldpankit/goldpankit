cfa8e626808af61c0b50af1a511511a9ba61494d
Tianxidev <super@tianxidev.wiki>
2024-11-01 19:16:34 +0800

    refactor: 移除名称转换工具函数中的工具名称打印

--- node-page/src/components/tools/toolbox/name-generator/name.generator.js
+++ node-page/src/components/tools/toolbox/name-generator/name.generator.js
@@ -1,8 +1,7 @@
 export default {
   _split(input) {
-    const toolFunName = "名称转换";
     if (typeof input !== "string") {
-      throw new Error(`[${toolFunName}] Invalid input type=${typeof input}`);
+      throw new Error(`Invalid input type=${typeof input}`);
     }
     return input
       .replace(/[\s_-]+/g, "|")
